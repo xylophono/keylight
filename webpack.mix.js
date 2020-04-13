420f92689a874e66cfa920109fd55ce5243af030
xylophono <git@xylophono.com>
2020-04-13 03:59:27 +0100

    dist to docs rename

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
 
-mix.js('src/js/app.js', 'dist/resources/app.js')
-   .sass('src/scss/app.scss', 'dist/resources/app.css')
-   .setPublicPath('dist');
+mix.js('src/js/app.js', 'docs/resources/app.js')
+   .sass('src/scss/app.scss', 'docs/resources/app.css')
+   .setPublicPath('docs');
